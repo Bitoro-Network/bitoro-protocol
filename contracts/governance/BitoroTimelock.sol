2245e40d433fb69528499c63e0c5d604b94c03ce
sniper365 <devtops2050@gmail.com>
2023-10-02 15:02:31 -0500

    refactor

---
+++ contracts/governance/BitoroTimelock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// This file just references TransparentUpgradeableProxy
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+
+contract BitoroTimelock is TimelockControllerUpgradeable {
+    function initialize(uint256 minDelay, address[] memory proposers, address[] memory executors) external initializer {
+        __TimelockController_init(minDelay, proposers, executors);
+    }
+}
